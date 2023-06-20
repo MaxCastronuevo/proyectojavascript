96d335ea38a25ae432a9c1758812e5e5b2d5c219
Max Castronuevo <mcastronue@outlook.com>
2023-06-20 11:28:26 -0300

    actualizacion de codigo array y push

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function solicitarNombre() {
     function mostrarProductos() {
     console.log("Productos disponibles:");
     console.log("1. Camiseta - $2000");
-    console.log("2. Pantalón - $4000");
+    console.log("2. Pantalon - $4000");
     console.log("3. Zapatos - $9000");
     return parseInt(prompt("Seleccione un producto o Ingrese 0 para finalizar:"));
   }
@@ -14,6 +14,7 @@ function solicitarNombre() {
     let nombreUsuario = solicitarNombre();
     let productoSeleccionado;
     let costoTotal = 0;
+    let productosSeleccionados = [];
   
     do {
       productoSeleccionado = mostrarProductos();
@@ -21,14 +22,17 @@ function solicitarNombre() {
       switch (productoSeleccionado) {
         case 1:
           costoTotal += 2000;
+          productosSeleccionados.push("Camiseta");
           console.log("Has seleccionado una camiseta.");
           break;
         case 2:
           costoTotal += 4000;
-          console.log("Has seleccionado un pantalón.");
+          productosSeleccionados.push("Pantalon")
+          console.log("Has seleccionado un pantalon.");
           break;
         case 3:
           costoTotal += 9000;
+          productosSeleccionados.push("Zapatos")
           console.log("Has seleccionado unos zapatos.");
           break;
         case 0:
@@ -40,8 +44,9 @@ function solicitarNombre() {
       }
     } while (productoSeleccionado !== 0);
   
+    console.log("Productos seleccionados por " + nombreUsuario + ": " + productosSeleccionados.join(", "));
     console.log("Costo total de los productos seleccionados por " + nombreUsuario + ": $" + costoTotal);
-  }
+}
   
-  calcularCostoTotal();
+calcularCostoTotal();
   
